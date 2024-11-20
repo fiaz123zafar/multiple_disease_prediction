26523f9c9fee5ade32b31d8c5b1d62abd46fece8
fiaz123zafar <fiazzafar1357@gmail.com>
2024-11-20 21:39:39 +0530

    Update app.py

--- disease/app.py
+++ disease/app.py
@@ -2,8 +2,14 @@
 import pandas as pd
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
+from st.sidebar.selectbox import option_menu 
 
+option = st.sidebar.selectbox(
+    'Choose an option',
+    ['disease\breast_cancer_model.pkl', 'disease\diabetes_model.pkl', 'disease\heart_disease_model.pkl']
+)
+
+st.write('You selected:', option)
 
 #loading the models
 diabetes = pickle.load(open("diabetes_model.pkl", "rb"))
